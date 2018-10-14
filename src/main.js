fed26e48d2c64ac311fdca62187c006c41b16696
Niall Ainsworth <nainsworth@guidewire.com>
2018-10-14 19:00:01 +0100

    change to absolute url

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
-Vue.http.options.root = 'https://niallains.github.io/AstroNotes/'
+Vue.http.options.root = 'https://niallains.github.io/AstroNotes/dist/'
 
 new Vue({
   router,
